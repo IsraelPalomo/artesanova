6ea3936ba253150b183f2edf858e3aa7b8685cfb
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-10-10 12:23:23 +0200

    Añadida ram al header a falta de configurar la card

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles/main.scss";
 import "./main.css";
+
+//images
+
+import imgSrc1 from "./static/rama_header.png";
+
+const img1 = document.querySelector(".header__img--rama");
+img1.src = imgSrc1;

83e3e5165e670c4308fbf784144c226912ba9a29
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-10-10 19:31:58 +0200

    Header terminado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import "./styles/main.scss";
 import "./main.css";
-
-//images
-
 import imgSrc1 from "./static/rama_header.png";
+import imgSrc2 from "./static/Producto1.png";
+import imgSrc3 from "./static/Producto2.png";
+import imgSrc4 from "./static/Producto3.png";
 
 const img1 = document.querySelector(".header__img--rama");
 img1.src = imgSrc1;
+
+const img2 = document.querySelector(".header__img--1");
+img2.src = imgSrc2;
+const img3 = document.querySelector(".header__img--2");
+img3.src = imgSrc3;
+const img4 = document.querySelector(".header__img--3");
+img4.src = imgSrc4;

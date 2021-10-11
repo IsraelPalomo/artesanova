55984d410e17afb145aeff1c96239c3c59661a69
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-10-11 13:30:06 +0200

    Acabada seccion productos

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import imgSrc1 from "./static/rama_header.png";
 import imgSrc2 from "./static/Producto1.png";
 import imgSrc3 from "./static/Producto2.png";
 import imgSrc4 from "./static/Producto3.png";
+import imgSrc5 from "./static/Producto4.png";
+import imgSrc6 from "./static/acuarela__productos.png";
+
+const img8 = document.querySelector(".acuarela__productos");
+img8.src = imgSrc6;
+
+const img5 = document.querySelector(".main__productos__img--1");
+img5.src = imgSrc2;
+const img6 = document.querySelector(".main__productos__img--2");
+img6.src = imgSrc5;
+const img7 = document.querySelector(".main__productos__img--3");
+img7.src = imgSrc3;
 
 const img1 = document.querySelector(".header__img--rama");
 img1.src = imgSrc1;
-
 const img2 = document.querySelector(".header__img--1");
 img2.src = imgSrc2;
 const img3 = document.querySelector(".header__img--2");
